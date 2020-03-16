a56740c3232a298138f3bcffcb0d52bc0e7a9d93
Viktor Krav4uk Home <viktorkrav4uk87@gmail.com>
2020-03-16 22:34:39 +0200

    Velocity

--- c11.js
+++ c11.js
@@ -14,15 +14,30 @@ const c11 = () => {
   intreval = setInterval(() => {
     let timePassed = Date.now() - start;
 
+    //https://github.com/finnursig/VideoScroller/blob/master/src/EasingFunctions.js
+
     if (timePassed < duration * 3) {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      const timeDivide = timePassed / duration;
-      const x1 = timeDivide * 400;
-      const x2 = Math.pow(timeDivide, 2) * 400;
-      const x3 = Math.pow(timeDivide, 3) * 400;
-      const x4 = (1 - Math.sin(Math.acos(timeDivide))) * 400;
-      const x5 = (1 - Math.sin(((1 - timeDivide) * Math.PI) / 2)) * 400;
-      const x6 = (1 - Math.sin(((1.3 - timeDivide) * Math.PI) / 2)) * 400;
+      let t = timePassed / duration;
+      let t5 = timePassed / duration;
+      let t6 = timePassed / duration;
+      // без easing, без прискорення
+      let x1 = t * 400;
+      // прискорення з нульової швидкості
+      let x2 = Math.pow(t, 2) * 400;
+      // тройне прискорення з нульової швидкості
+      let x3 = Math.pow(t, 3) * 400;
+      // уповільнення до нульової швидкості
+      let x4 = (t*(2-t)) * 400;
+      //прискорення до півдороги, потім уповільнення
+      let x5 = (t5<.5 ? 2*t5*t5 : -1+(4-2*t5)*t5) * 400
+      // уповільнення до нульової швидкості
+      const x6 = ((--t6)*t6*t6+1) * 400
+
+      // рух по різних функціях
+      //const x4 = (1 - Math.sin(Math.acos(t))) * 400;
+      //const x5 = (1 - Math.sin(((1 - t) * Math.PI) / 2)) * 400;
+      //const x6 = (1 - Math.sin(((1.3 - t) * Math.PI) / 2)) * 400;
 
       ctx.fillRect(x1, 25 - 4, 8, 8);
       ctx.fillRect(x2, 50 - 4, 8, 8);
--- dist/canvas.e31bb0bc.js
+++ dist/canvas.e31bb0bc.js
@@ -505,17 +505,29 @@ var c11 = function c11() {
   ctx.strokeStyle = "blue";
   ctx.fillStyle = "blue";
   intreval = setInterval(function () {
-    var timePassed = Date.now() - start;
+    var timePassed = Date.now() - start; //https://github.com/finnursig/VideoScroller/blob/master/src/EasingFunctions.js
 
     if (timePassed < duration * 3) {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      var timeDivide = timePassed / duration;
-      var x1 = timeDivide * 400;
-      var x2 = Math.pow(timeDivide, 2) * 400;
-      var x3 = Math.pow(timeDivide, 3) * 400;
-      var x4 = (1 - Math.sin(Math.acos(timeDivide))) * 400;
-      var x5 = (1 - Math.sin((1 - timeDivide) * Math.PI / 2)) * 400;
-      var x6 = (1 - Math.sin((1.3 - timeDivide) * Math.PI / 2)) * 400;
+      var t = timePassed / duration;
+      var t5 = timePassed / duration;
+      var t6 = timePassed / duration; // без easing, без прискорення
+
+      var x1 = t * 400; // прискорення з нульової швидкості
+
+      var x2 = Math.pow(t, 2) * 400; // тройне прискорення з нульової швидкості
+
+      var x3 = Math.pow(t, 3) * 400; // уповільнення до нульової швидкості
+
+      var x4 = t * (2 - t) * 400; //прискорення до півдороги, потім уповільнення
+
+      var x5 = (t5 < .5 ? 2 * t5 * t5 : -1 + (4 - 2 * t5) * t5) * 400; // уповільнення до нульової швидкості
+
+      var x6 = (--t6 * t6 * t6 + 1) * 400; // рух по різних функціях
+      //const x4 = (1 - Math.sin(Math.acos(t))) * 400;
+      //const x5 = (1 - Math.sin(((1 - t) * Math.PI) / 2)) * 400;
+      //const x6 = (1 - Math.sin(((1.3 - t) * Math.PI) / 2)) * 400;
+
       ctx.fillRect(x1, 25 - 4, 8, 8);
       ctx.fillRect(x2, 50 - 4, 8, 8);
       ctx.fillRect(x3, 75 - 4, 8, 8);
