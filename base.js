cc48a4a98b8e6cedc2c1e90b2f1a8c23f0e7e399
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2021-04-21 22:28:12 +0300

    add context

--- base.js
+++ base.js
@@ -1,4 +1,4 @@
-
+console.groupCollapsed('ЗАМИКАННЯ')
 console.log('Змикання виникає коли ми з оннієї функції повертаємо іншу функцію')
 console.log('тоді кажемо що нова функція замкнута на область видимості батьківської')
 
@@ -13,7 +13,6 @@ console.log("https://google.com", comUrl('google'));
 console.log("https://google.net", netUrl('google'));
 
 console.log(urlGenerator('biz')('apple')) //https://apple.biz
-console.log('');
 
 function counter() {
 	let count = 0
@@ -24,7 +23,38 @@ function counter() {
 const useConunter = counter()
 console.log('useConunter', useConunter());
 console.log('useConunter2', useConunter());
+console.groupEnd();
+console.log('------------------------------');
 
-console.log('------------------------------'); console.log('');console.log('');
+console.group('КОНТЕКСТ')
+
+function outputThis() {
+	console.log('this', this)
+}
+outputThis() // Window{...}
+
+const person = {
+	name: 'Viktor',
+	age: 33,
+	sayHello: outputThis,
+	sayHelloWindow: outputThis.bind(window), // в bind передали контекст
+	logInfo: function() {
+		console.log('logInfo name', this.name)
+	},
+}
+person.sayHello() // {name: "Viktor", age: 33, sayHello: ƒ}
+person.sayHelloWindow() // Window{...}
+person.logInfo() // Viktor
+
+const cat = {
+	name: 'Murzik',
+	age: 1
+}
+
+const catPerson = person.logInfo.bind(cat)
+catPerson() // Murzik
+
+console.groupEnd();
+console.log('------------------------------');
 
 	
