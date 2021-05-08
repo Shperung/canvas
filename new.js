d9f40dbe478bb362eadf4b3512557a646384c79e
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2021-05-08 16:18:42 +0300

    replase all

---
+++ new.js
@@ -0,0 +1,8 @@
+console.groupCollapsed('replaceAll')
+
+const str = 'PHP test PHP test PHP';
+console.log('str.replace - ', str.replace("PHP", "JS"))
+console.log('str.replaceAll - ', str.replaceAll("PHP", "JS"))
+
+console.groupEnd
+
